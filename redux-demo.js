730d304a542e3d7c70de490ed5127e621bd2bb3c
Rajat Choudhary <rajatch133@gmail.com>
2023-08-23 23:57:56 +0530

    created action and dispatched them for reducer

--- redux-demo.js
+++ redux-demo.js
@@ -1,9 +1,34 @@
 const redux = require('redux');
 
-const counterReducer = (state, action) => {
-    return {
-        counter: 0
-    };
+const counterReducer = (state = {counter: 0}, action) => {
+    if(action.type==="increment"){
+        return {
+            counter: state.counter + 1,
+        };
+    }
+    if(action.type === "decrement"){
+        return {
+            counter: state.counter - 1,
+        }
+    }
+
+    return state;
+    
 }
 
-const store = redux.createStore();
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+    const latestState = store.getState();
+    // getState() gives us latest state snapshot after it was updated
+    console.log(latestState);
+};
+
+// to add subscriber it is inbuilt
+store.subscribe(counterSubscriber);
+
+// action
+
+store.dispatch({ type: 'increment'});
+
+store.dispatch({ type: 'decrement'});
